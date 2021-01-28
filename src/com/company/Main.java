22f0b067bff07f6c3e858f5d09119d312a4889fe
Dinara9786 <knowledge9786@gmail.com>
2021-01-27 21:41:55 -0500

    Planet1 is ready to be merged

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,14 @@ public class Main {
         FactorX.Sun = 1;
         FactorX.Moon = 2;
         FactorX.planets = 2;
-
         SUN troyo = new SUN();
         troyo.Heat = "56748k";
         troyo.temperature = "347878k";
         troyo.Radius = "99899898km";
+
+        Planet1 Fearo = new Planet1();
+        Fearo.Radius = "87889k";
+        Fearo.color = "red";
 
     }
 }
