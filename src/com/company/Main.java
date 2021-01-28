8dc4e847ae2c1acf3788ab2424606618f5cc18b8
Dinara9786 <66706614+Dinara9786@users.noreply.github.com>
2021-01-27 22:25:58 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	    
+	    Dinara
 
         SolarSystem FactorX = new SolarSystem();
         FactorX.Sun = 1;
